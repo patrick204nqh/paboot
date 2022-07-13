e94324d4f9f9bffd0860b4420a914d75434a9815
Nguyen Quang Huy <patrick204nqh@gmail.com>
2022-07-13 22:48:11 +0700

    Setup admin

--- src/main/java/com/patrick204nqh/paboot/PabootApplication.java
+++ src/main/java/com/patrick204nqh/paboot/PabootApplication.java
@@ -2,8 +2,10 @@ package com.patrick204nqh.paboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
+@EnableAdminServer
 public class PabootApplication {
 
 	public static void main(String[] args) {
